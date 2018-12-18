0081977d32250a5b005042f6aa4014e9cd633a9a
Vijay kumar <vk45800@gmail.com>
2018-12-18 18:04:02 +0530

    Add files via upload

---
+++ fetchDetail.php
@@ -0,0 +1,9 @@
+<?php
+
+// PHP SDK: https://github.com/razorpay/razorpay-php
+use Razorpay\Api\Api;
+$api = new Api("rzp_test_sEA6555roiMFZP", "");
+
+$api->utility->verifyWebhookSignature($webhookBody, $webhookSignature, $webhookSecret);
+
+?>
