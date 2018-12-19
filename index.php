b9c5e2620aea04b702ee7dfd820bda633f50485a
Vijay kumar <vk45800@gmail.com>
2018-12-19 10:24:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,14 @@ Hello
 Hello 2
 
 <?php
-use razorpay\Api\Api;
+use Razorpay\Api\Api;
 $api = new Api('rzp_test_sEA6555roiMFZP', 'PQLT3VqL00JLZbJ0GGy4uNlq');
+
+$api->utility->verifyWebhookSignature($webhookBody, $webhookSignature, $webhookSecret);
+
+
+
+
 $payment = $api->payment->fetch($_REQUEST['billno']);
 echo $payment->amount;
 print_r($payment->notes);
