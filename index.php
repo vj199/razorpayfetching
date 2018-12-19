781e5116b8377d3e1bfb04dc0ace0bff81128c46
Vijay kumar <vk45800@gmail.com>
2018-12-19 10:02:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,3 @@ echo $payment->amount;
 print_r($payment->notes);
 echo "abcde";
 ?>
-
-<!-- <?php
-curl -u https://rzp_test_sEA6555roiMFZP:PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay.com/v1/invoices \
-   -X POST \
-   --data "amount=500" \
-https://rzp_test_sEA6555roiMFZP:PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay.com/v1/capture
-?> -->
