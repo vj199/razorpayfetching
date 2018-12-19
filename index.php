0b81de00885ec9f2e53ec4932f3516faf39f61ef
Vijay kumar <vk45800@gmail.com>
2018-12-19 09:40:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,4 +9,5 @@ $api = new Api('rzp_test_sEA6555roiMFZP', 'PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay
 $payment = $api->payment->fetch($_REQUEST['billno']);
 echo $payment->amount;
 print_r($payment->notes);
+echo "abcde";
 ?>
