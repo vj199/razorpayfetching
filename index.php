8e04a9c899df44276b340e78ad86c8fe8b7a76e8
Vijay kumar <vk45800@gmail.com>
2018-12-19 09:44:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Hello 2
 
 <?php
 include 'vendor/razorpay/razorpay/Razorpay.php';
-use Razorpay\Api\Api;
+use Razorpay\Api;
 $api = new Api('rzp_test_sEA6555roiMFZP', 'PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay.com/v1/invoices');
 $payment = $api->payment->fetch($_REQUEST['billno']);
 echo $payment->amount;
