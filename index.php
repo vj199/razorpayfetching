221290fc8a28f012a0de88d3664a7d2524508042
Vijay kumar <vk45800@gmail.com>
2018-12-19 10:00:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,16 @@ Hello 2
 <?php
 include 'vendor/razorpay/razorpay/Razorpay.php';
 use razorpay\razorpay\Api;
-$api = new Api('rzp_test_sEA6555roiMFZP', 'PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay.com/v1/invoices');
+$api = new Api('rzp_test_sEA6555roiMFZP', 'PQLT3VqL00JLZbJ0GGy4uNlq');
 $payment = $api->payment->fetch($_REQUEST['billno']);
 echo $payment->amount;
 print_r($payment->notes);
 echo "abcde";
 ?>
+
+<!-- <?php
+curl -u https://rzp_test_sEA6555roiMFZP:PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay.com/v1/invoices \
+   -X POST \
+   --data "amount=500" \
+https://rzp_test_sEA6555roiMFZP:PQLT3VqL00JLZbJ0GGy4uNlq@api.razorpay.com/v1/capture
+?> -->
